5482552d8adb767c007794679d0b54b8d2e92fdc
Kiana-ak <kiana.akbari8@gmail.com>
2025-04-28 11:49:36 +1200

    resolve conflicts

--- hinterest/src/App.js
+++ hinterest/src/App.js
@@ -1,14 +1,8 @@
 import './App.css';
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { AuthProvider } from './contexts/AuthContext.js';
-import { ThemeProvider } from './ThemeContext';
-import Signup from './components/Signup';
-import Login from './components/Login';
-import ProtectedRoute from './auth/ProtectedRoute';
-import ForgotPassword from './components/ForgetPasword';
-import UpdateProfile from './components/UpdateProfile';
-import FlashCards from './components/Flashcards';
+import Flashcards from './components/Flashcards';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
 import LoginPage from './pages/LoginPage';
 import Home from './pages/Home';
 import CalendarLogin from './pages/CalendarLogin';
@@ -16,45 +10,20 @@ import CalendarPage from './pages/CalendarPage';
 
 function App() {
   return (
-    <Router>
-      <AuthProvider>
-        <ThemeProvider>
-          <div className="w-100" style={{ maxWidth: "400px", margin: "0 auto" }}>
-            <Routes>
-              <Route path="/" element={<LoginPage />} />
-              <Route path="/home" element={
-                <ProtectedRoute>
-                  <Home />
-                </ProtectedRoute>
-              } />
-              <Route path="/calendar-login" element={
-                <ProtectedRoute>
-                  <CalendarLogin />
-                </ProtectedRoute>
-              } />
-              <Route path="/calendar" element={
-                <ProtectedRoute>
-                  <CalendarPage />
-                </ProtectedRoute>
-              } />
-              <Route path="/update-profile" element={
-                <ProtectedRoute>
-                  <UpdateProfile />
-                </ProtectedRoute>
-              } />
-              <Route path="/flashcards" element={
-                <ProtectedRoute>
-                  <FlashCards />
-                </ProtectedRoute>
-              } />
-              <Route path="/signup" element={<Signup />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/forgot-password" element={<ForgotPassword />} />
-            </Routes>
-          </div>
-        </ThemeProvider>
-      </AuthProvider>
-    </Router>
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/calendar-login" element={<CalendarLogin />} />
+          <Route path="/calendar" element={<CalendarPage />} />
+        </Routes>
+        <main>
+          <Flashcards />
+        </main>
+      </BrowserRouter>
+    </div>
   );
 }
 
